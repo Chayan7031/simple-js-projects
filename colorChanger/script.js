8582950db6c10f76bd9c2aac13c960a2bec01708
Chayan <chayansamanta406@gmail.com>
2025-06-26 22:49:10 +0530

    new project

---
+++ colorChanger/script.js
@@ -0,0 +1,25 @@
+let box1 = document.querySelector("#box1")
+let box2 = document.getElementById("box2")
+let box3 = document.getElementById("box3")
+let box4 = document.getElementById("box4")
+let btn = document.getElementById("btn")
+
+box1.addEventListener("click", function(){
+    document.body.style.backgroundColor = "gray"
+})
+
+box2.addEventListener("click", function(){
+    document.body.style.backgroundColor = "blue"
+})
+
+box3.addEventListener("click", function(){
+    document.body.style.backgroundColor = "brown"
+})
+
+box4.addEventListener("click", function(){
+    document.body.style.backgroundColor = "yellow"
+})
+
+btn.addEventListener("click", function(){
+    document.body.style.backgroundColor = "white"
+})
